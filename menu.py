bab62bbc6a6434a33c6f4f8b052309ad77343ea4
HTutiya <henrique.kt1@puccampinas.edu.br>
2026-04-07 10:21:05 -0300

    int para string

--- menu.py
+++ menu.py
@@ -1,151 +1,151 @@
-inicio = 0
-while inicio != 3:
+inicio = "
+while inicio != "3":
     print(f"\nInício")
     print(f"\n----------------------------Sistema de Votação----------------------------------")
     print("\n1-Votação\n2-gerenciamento\n3-sair\n")
-    inicio= int(input("Escolha a opção desejada:"))
+    inicio= input("Escolha a opção desejada:")
     match inicio:
-        case 1:
-            Votação=0
-            while Votação!=4:
+        case "1":
+            Votação=""
+            while Votação!="4":
                 print(f"\n----------------------------Votação---------------------------------------------")
                 print("\n1-Auditoria\n2-Abrir sistema de votação\n3-Resultados\n4-Voltar\n")
-                Votação=int(input("Escolha a opção desejada:"))
+                Votação=input("Escolha a opção desejada:")
                 match Votação:
-                    case 1:
-                        Auditoria=0
-                        while Auditoria!=3:
+                    case "1":
+                        Auditoria=""
+                        while Auditoria!="3":
                             print(f"\n----------------------------Auditoria-------------------------------------------")
                             print("\n1-Protocolos de votação\n2-Logs de ocorrência\n3-Voltar\n")
-                            Auditoria=int(input("Escolha a opção desejada:"))
+                            Auditoria=input("Escolha a opção desejada:")
                             match Auditoria:
-                                case 1:
+                                case "1":
                                     pass
-                                case 2:
+                                case "2":
                                     pass
-                                case 3:
+                                case "3":
                                     print("Voltando...\n")
                                 case _:
                                     print("Opção inválida\n")
-                    case 2:
-                        Abrir_sistema=0
-                        while Abrir_sistema!=3:
+                    case "2":
+                        Abrir_sistema=""
+                        while Abrir_sistema!="3":
                             print(f"\n------------------------------Sistema de Votação--------------------------------")
                             print("\n1-Votar\n2-Encerrar sistema de votação\n3-Voltar\n")
-                            Abrir_sistema=int(input("Escolha a opção desejada:"))      
+                            Abrir_sistema=input("Escolha a opção desejada:")      
                             match Abrir_sistema:
-                                case 1:
-                                    Votar=0
-                                    while Votar!=1:
+                                case "1":
+                                    Votar=""
+                                    while Votar!="1":
                                         print(f"\n----------------------------Votação---------------------------------------------")
                                         print("\n1-Cancelar voto\n2-Confirmar voto\n")
-                                        Votar=int(input("Escolha a opção desejada:"))
+                                        Votar=input("Escolha a opção desejada:")
                                         match Votar:
-                                            case 1:
+                                            case "1":
                                                 print("Voto cancelado\n")
-                                            case 2:
+                                            case "2":
                                                 print("Voto confirmado\n")
-                                                Votar=1
-                                                Abrir_sistema=3
+                                                Votar="1"
+                                                Abrir_sistema="3"
                                             case _:
                                                 print("Opção inválida\n")
-                                case 2:
-                                    Encerrar=1
-                                    while Encerrar==1:
+                                case "2":
+                                    Encerrar="1"
+                                    while Encerrar=="1":
                                         print(f"\n----------------------------Encerrar Votação------------------------------------")
                                         print("\n1-Não encerrar\n2-Encerrar\n3-Voltar\n")
-                                        Encerrar=int(input("Escolha a opção desejada:"))
+                                        Encerrar=input("Escolha a opção desejada:")
                                         match Encerrar:
-                                            case 1 :
+                                            case "1" :
                                                 print("Sistema de votação não encerrado\n")
-                                            case 2:
+                                            case "2":
                                                 print("Encerrando sistema de votação...\n")
                                                 Abrir_sistema=3  
-                                            case 3:
+                                            case "3":
                                                 print("Voltando...\n")      
                                             case _:
                                                 print("Opção inválida\n")                  
-                                case 3:
+                                case "3":
                                     print("Voltando...\n")
                                 case _:
                                     print("Opção inválida\n")                    
-                    case 3:
-                        Resultados=0
-                        while Resultados!=5:
+                    case "3":
+                        Resultados=""
+                        while Resultados!="5":
                             print(f"\n----------------------------Resultados------------------------------------------")
                             print("\n1-Boletim de urna\n2-Estatística de comparecimento\n3-Validação de integridade\n4-Votos por partido\n5-Voltar\n")
-                            Resultados=int(input("Escolha a opção desejada:"))
+                            Resultados=input("Escolha a opção desejada:")
                             match Resultados:
-                                case 1:
+                                case "1":
                                     pass
-                                case 2:
+                                case "2":
                                     pass
-                                case 3:
+                                case "3":
                                     pass
-                                case 4:
+                                case "4":
                                     pass
-                                case 5:
+                                case "5":
                                     print("Voltando...\n")
                                 case _:
                                     print("Opção inválida\n")
-                    case 4:
+                    case "4":
                        print("Voltando...")
                     case _:
                         print("Opção inválida\n")
-        case 2:
-            Gerenciamento=0
-            while Gerenciamento!=3:
+        case "2":
+            Gerenciamento=""
+            while Gerenciamento!="3":
                 print(f"\n----------------------------Gerenciamento---------------------------------------")
                 print("\n1-Eleitores\n2-Candidatos\n3-Voltar\n")
-                Gerenciamento=int(input("Escolha a opção desejada:"))
+                Gerenciamento=input("Escolha a opção desejada:")
                 match Gerenciamento:
-                    case 1:
-                        Eleitores=0
-                        while Eleitores!=6:
+                    case "1":
+                        Eleitores=""
+                        while Eleitores!="6":
                             print(f"\n----------------------------Eleitores-------------------------------------------")
                             print("\n1-Remoção de eleitores\n2-Buscar eleitores\n3-Listagen de eleitores\n4-Editar dados de eleitores\n5-Cadastrar eleitores\n6-Voltar\n")
-                            Eleitores=int(input("Escolha a opção desejada:"))
+                            Eleitores=input("Escolha a opção desejada:")
                             match Eleitores:
-                                case 1:
+                                case "1":
                                     pass
-                                case 2:
+                                case "2":
                                     pass
-                                case 3:
+                                case "3":
                                     pass
-                                case 4:
+                                case "4":
                                     pass
-                                case 5:
+                                case "5":
                                     pass
-                                case 6:
+                                case "6":
                                     print("Voltando...\n")
                                 case _:
                                     print("Opção inválida\n")
-                    case 2:
-                        Candidatos=0
-                        while Candidatos!=6:
+                    case "2":
+                        Candidatos=""
+                        while Candidatos!="6":
                             print(f"\n----------------------------Candidatos------------------------------------------")
                             print("\n1-Buscar candidato\n2-Cadastro de candidatos\n3-Editar dados de candidatos\n4-Remover candidato\n5-Listar candidatos\n6-Voltar\n")
-                            Candidatos=int(input("Escolha a opção desejada:"))
+                            Candidatos=input("Escolha a opção desejada:")
                             match Candidatos:
-                                case 1:
+                                case "1":
                                     pass
-                                case 2:
+                                case "2":
                                     pass
-                                case 3:
+                                case "3":
                                     pass
-                                case 4:
+                                case "4":
                                     pass
-                                case 5:
+                                case "5":
                                     pass
-                                case 6:
+                                case "6":
                                     print("Voltando...")
                                 case _:
                                     print("Opção inválida\n")
-                    case 3:
+                    case "3":
                         print("Voltando...\n")
                     case _:
                         print("Opção inválida\n")
-        case 3:
+        case "3":
             print("Saindo...")
         case _:
             print("Opção inválida\n")
