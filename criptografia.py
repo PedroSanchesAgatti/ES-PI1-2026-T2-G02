0596449227ef97f6bd324147b58b4fd20a4ec765
HTutiya <henrique.kt1@puccampinas.edu.br>
2026-04-22 23:40:49 -0300

    Add files via upload

---
+++ criptografia.py
@@ -0,0 +1,45 @@
+def criptografia(palavra):
+    lista = ["A","B","C","D","E","F","G","H","I","J","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9"]
+    chave = [[16, 18], [15, 17]]
+    x = [[], []]
+    cont = 0
+
+    if len(palavra) % 2 == 1:
+        palavra += "A"
+
+    for letra in palavra:
+        if cont % 2 == 0:
+            x[0].append(lista.index(letra))
+        else:
+            x[1].append(lista.index(letra))
+        cont += 1
+    matrix=multiplicacao_matrizes(chave,x)
+    matrix=[[x%36 for x in row]for row in matrix]
+    resultado=""
+    for j in range(len(matrix[0])):
+        resultado+=lista[matrix[0][j]]
+        resultado+=lista[matrix[1][j]]
+    if len(palavra)%2 ==1:
+        resultado+="K"
+    print(resultado)
+
+
+def multiplicacao_matrizes(matrix_1, matrix_2):
+    resultado = []
+    for i in range(len(matrix_1)):
+        linha = []
+        for j in range(len(matrix_2[0])):
+            soma = 0
+            for k in range(len(matrix_2)):
+                soma += matrix_1[i][k] * matrix_2[k][j]
+            linha.append(soma)
+        resultado.append(linha)
+
+    return resultado
+
+
+
+
+
+
+criptografia(input("Digite a palavra: ").upper())
